278a6c64f16f0be17b45043b6ba48ace4b75b63d
nftcoffins <nftcoffins@gmail.com>
2021-06-06 11:49:13 +0300

    rules

---
+++ client/src/components/rules/rules.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import empty from '../../../images/emptyCoffin.png'
+import withAttributes from '../../../images/withAttributes.png'
+import fullCoffin from '../../../images/fullCoffin.png'
+
+import styles from './rules.scss'
+
+type Props = {
+    className: string
+}
+
+export function Rules({ className }: Props) {
+    return (
+        <div className={classNames(styles.rules, className)}>
+            <div className={styles.title}>just three rules</div>
+
+            <div className={styles.rulesBlock}>
+                <div className={styles.rule}>
+                    <img src={empty} />
+                    <div className={styles.subtitle}>Empty Coffin</div>
+                    <div className={styles.description}>
+                        When the character is alive in the real world, the coffin is empty.
+                    </div>
+                </div>
+
+                <div className={styles.rule}>
+                    <img src={withAttributes} />
+                    <div className={styles.subtitle}>With Attributes</div>
+                    <div className={styles.description}>
+                        The character is still alive but filled with attributes related to the character's life.
+                    </div>
+                </div>
+
+                <div className={styles.rule}>
+                    <img src={fullCoffin} />
+                    <div className={styles.subtitle}>Full Coffin</div>
+                    <div className={styles.description}>
+                        The coffin lid slammed. This is a full coffin behind glass. It Will never be changed attributes
+                        related to the character's life.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/components/shared/openSeaLink/OpenSeaLink.tsx
+++ client/src/components/shared/openSeaLink/OpenSeaLink.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+import { OPEN_SEA_URL } from '../../../constants'
 import coffinImage from '../../../../images/coffin.png'
 import styles from './OpenSeaLink.scss'
 
 export function OpenSeaLink() {
     return (
         <div className={styles.openSeaLink}>
-            <a href="#">Buy on OpenSea</a>
+            <a href={OPEN_SEA_URL} target="_blank">
+                Buy on OpenSea
+            </a>
             <img src={coffinImage} />
         </div>
     )
