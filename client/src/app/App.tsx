bdaf16dae88a3686f98e74e84ae5e546c2e596a5
nftcoffins <nftcoffins@gmail.com>
2021-06-09 18:48:47 +0300

    hash router

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { useAppDispatch } from '../shared/hooks'
 import { Header } from '../components/header/Header'
